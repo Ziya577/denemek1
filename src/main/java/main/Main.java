94944735f8a3b70ea34d0cb6b0a3e7c2d0aaf34b
Ziya577 <ziyasirinov577@gmail.com>
2024-05-23 13:00:06 +0400

    yeni

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(6);
+        System.out.println(3);
+        System.out.println(2);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(1);
+
+    }
+
+    public static int test(int a) {
+
+        return 0;
+    }
+
+    public static long test(int a,long b){
+        return 2;
+
+
+    }
+}
